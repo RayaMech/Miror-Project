bec2d8d4c708b6095b705ee3079182a75546c00a
RayaMech <rayane.mechik.etu@univ-lille.fr>
2021-12-14 11:59:41 +0100

    Ajout fichier lire CSV

--- actuator/actuator.ino
+++ actuator/actuator.ino
@@ -50,11 +50,11 @@ int hauteur;
 
 void setup(){
   //pinMode(A9,INPUT);
- // Serial.begin(9600);
+  Serial.begin(9600);
   actuator1.attach(A9); //attach the actuator to Arduino pin9
   //actuator2.attach(A8); //attach the actuator to Arduino pin6
   //actuator3.attach(A8);
-  actuator1.writeMicroseconds(2000); //give the actuator a 2ms pulse to retract the arm (1000us=1ms)
+  actuator1.writeMicroseconds(1900); //give the actuator a 2ms pulse to retract the arm (1000us=1ms)
   //delay(3000);
   //actuator2.writeMicroseconds(2000);
   //actuator3.writeMicroseconds(2000);
@@ -87,9 +87,9 @@ void loop(){
    delay(3000); // the actuator takes>2s to extend/retract when loaded - give it plenty of time
   // exit(0);
    
-   //actuator1.writeMicroseconds(2000); //2ms pulse to retract the arm
+   actuator1.writeMicroseconds(1900); //2ms pulse to retract the arm
    //actuator2.writeMicroseconds(2000);
-   //delay(2000);
+   delay(3000);
    //Serial.println(valeur1);      // affichage de la valeur via le port série
    //Serial.println("SERVO1 ASSIS");
    //Serial.println(valeur2);      // affichage de la valeur via le port série
@@ -102,9 +102,10 @@ void loop(){
   // Serial.println("SERVO ASSIS");
    //Serial.println(value2);
    //delay(6000);
-   //actuator3.writeMicroseconds(2000);
+   actuator1.writeMicroseconds(1500);
    
-   //delay(3000);
+   delay(3000);
+   exit(0);
 }
 
 
---
+++ lire_csv/lire_csv.ino
@@ -0,0 +1,22 @@
+const byte numChars = 32;
+char receivedChars[numChars];   // an array to store the received data
+
+boolean newData = false;
+
+void setup() {
+    Serial.begin(9600);
+    Serial.println("<Arduino is ready>");
+}
+
+void loop() {
+    recvWithEndMarker();
+    showNewData();
+}
+
+void recvWithEndMarker() {
+    static byte ndx = 0;
+    char endMarker = '\n';
+    char rc;
+    
+    while (Serial.available() > 0 && newData == false) {
+        rc = Serial.read();
